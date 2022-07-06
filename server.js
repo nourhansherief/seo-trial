11aa019f094af11e62f4850b8f9d0bb03767c165
Nourhan Sherif <Nourhan.Sherif@vodafone.com.eg>
2022-07-06 10:20:06 +0200

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const myBotList = rendertron.botUserAgents.concat(['googlebot', 'yolobot']);
+const rendertron = require('rendertron-middleware');
 
+app.use(
+  rendertron.makeMiddleware({
+    // use the extended bot list:
+    userAgentPattern: new RegExp(myBotList.join('|'), 'i'),
+    // replace this with the web address of your rendertron instance
+    proxyUrl: 'https://render-tron.appspot.com/render',
+  })
+);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/seo'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/seo/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/seo/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
