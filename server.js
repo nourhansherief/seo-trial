afd4263a5e5d5bdda84f4870f67298cdeaaa5d6b
Nourhan Sherif <Nourhan.Sherif@vodafone.com.eg>
2022-07-06 10:14:06 +0200

    first commit

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/seo'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/seo/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/seo/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
